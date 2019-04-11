beec547ee8e0bca090122a34faa3da4a1322dce5
luidna <luizgustav@gmail.com>
2019-04-11 00:04:21 -0300

    Agora comecando a colocar os metodos antigos

--- app/src/main/java/com/example/ahnahn_geometria/MainActivity.java
+++ app/src/main/java/com/example/ahnahn_geometria/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.lang.Math;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     Button button1, button_ddp;
@@ -30,11 +31,35 @@ public class MainActivity extends AppCompatActivity {
         EditText fieldPB = (EditText)findViewById(R.id.ddp2);
         String fieldPBValue = fieldPB.getText().toString();
 
-
+        String[] pontoA = fieldPAValue.split(@'-?\d');
 
         TextView respostaDDP = (TextView)findViewById(R.id.ddpr);
-        respostaDDP.setText(fieldPAValue);
+        respostaDDP.setText(pontoA[0]);
 
+    }
+
+    public static double dist2P(int x1, int y1, int x2, int y2)
+    {
+        int termoA = x1 - x2;
+        int termoB = y1 - y2;
+        int soma = (termoA * termoA) + (termoB * termoB);
+        double resultado = Math.sqrt(soma);
+
+        if (ehInteiro(resultado)){
+            System.out.println(String.valueOf((int)resultado));
+        } else {
+            DecimalFormat formatador = new DecimalFormat("#.0000");
+            System.out.println("√"+ soma + " ou "+ String.valueOf(formatador.format(resultado)));
+        }
+        return resultado;
+
+    }
+
+    public static Boolean ehInteiro(double val){
+        //confere se eh inteiro ao ver a string
+        String valOriginal = (String.valueOf(val));
+        String valInt = String.valueOf((int)val) + ".0";
+        return valOriginal.equals(valInt);
     }
 
 
